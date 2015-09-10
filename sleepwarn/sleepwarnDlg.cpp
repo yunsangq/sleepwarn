8a73e8744d38006be773653e60f113836ddbf3e4
yunsangq <yunsangq@gmail.com>
2015-09-11 00:19:01 +0900

    add eye/gaze tracking

--- sleepwarn/sleepwarnDlg.cpp
+++ sleepwarn/sleepwarnDlg.cpp
@@ -12,7 +12,13 @@
 #include "opencv2\imgproc\imgproc.hpp"
 
 #include <iostream>
+#include <queue>
 #include <stdio.h>
+#include <math.h>
+
+#include "constants.h"
+#include "findEyeCenter.h"
+#include "findEyeCorner.h"
 using namespace std;
 using namespace cv;
 
@@ -31,7 +37,9 @@ CascadeClassifier eyes_cascade;
 CascadeClassifier close_cascade;
 
 CString strPathName = NULL;
-
+cv::RNG rng(12345);
+cv::Mat debugImage;
+cv::Mat skinCrCbHist = cv::Mat::zeros(cv::Size(256, 256), CV_8UC1);
 // 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
 
 class CAboutDlg : public CDialogEx
@@ -128,12 +136,12 @@ BOOL CsleepwarnDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
 	// TODO: 여기에 추가 초기화 작업을 추가합니다.
-	/*
+	
 	m_capture = cvCreateCameraCapture(0);
 	if (!m_capture)
 		AfxMessageBox(_T("카메라가 없습니다."));
-	*/
-	m_capture = cvCreateFileCapture("C:\\Users\\Administrator\\Documents\\test.avi ");
+	
+	//m_capture = cvCreateFileCapture("C:\\Users\\Administrator\\Documents\\test.avi ");
 
 	if (!face_cascade.load(face_cascade_name)) AfxMessageBox(_T("Error loading face"));
 	if (!eyes_cascade.load(eyes_cascade_name)) AfxMessageBox(_T("Error loading eyes"));
@@ -226,6 +234,108 @@ void CsleepwarnDlg::OnDestroy()
 		cvReleaseCapture(&m_capture);
 }
 
+
+cv::Mat findSkin(cv::Mat &frame) {
+	cv::Mat input;
+	cv::Mat output = cv::Mat(frame.rows, frame.cols, CV_8U);
+
+	cvtColor(frame, input, CV_BGR2YCrCb);
+
+	for (int y = 0; y < input.rows; ++y) {
+		const cv::Vec3b *Mr = input.ptr<cv::Vec3b>(y);
+		//    uchar *Or = output.ptr<uchar>(y);
+		cv::Vec3b *Or = frame.ptr<cv::Vec3b>(y);
+		for (int x = 0; x < input.cols; ++x) {
+			cv::Vec3b ycrcb = Mr[x];
+			//      Or[x] = (skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) > 0) ? 255 : 0;
+			if (skinCrCbHist.at<uchar>(ycrcb[1], ycrcb[2]) == 0) {
+				Or[x] = cv::Vec3b(0, 0, 0);
+			}
+		}
+	}
+	return output;
+}
+
+
+Mat findEyes(cv::Mat frame_gray, cv::Rect face) {
+	cv::Mat faceROI = frame_gray(face);
+	cv::Mat debugFace = faceROI;
+
+	if (kSmoothFaceImage) {
+		double sigma = kSmoothFaceFactor * face.width;
+		GaussianBlur(faceROI, faceROI, cv::Size(0, 0), sigma);
+	}
+	//-- Find eye regions and draw them
+	int eye_region_width = face.width * (kEyePercentWidth / 100.0);
+	int eye_region_height = face.width * (kEyePercentHeight / 100.0);
+	int eye_region_top = face.height * (kEyePercentTop / 100.0);
+	cv::Rect leftEyeRegion(face.width*(kEyePercentSide / 100.0),
+		eye_region_top, eye_region_width, eye_region_height);
+	cv::Rect rightEyeRegion(face.width - eye_region_width - face.width*(kEyePercentSide / 100.0),
+		eye_region_top, eye_region_width, eye_region_height);
+
+	//-- Find Eye Centers
+	cv::Point leftPupil = findEyeCenter(faceROI, leftEyeRegion, "Left Eye");
+	cv::Point rightPupil = findEyeCenter(faceROI, rightEyeRegion, "Right Eye");
+	// get corner regions
+	cv::Rect leftRightCornerRegion(leftEyeRegion);
+	leftRightCornerRegion.width -= leftPupil.x;
+	leftRightCornerRegion.x += leftPupil.x;
+	leftRightCornerRegion.height /= 2;
+	leftRightCornerRegion.y += leftRightCornerRegion.height / 2;
+	cv::Rect leftLeftCornerRegion(leftEyeRegion);
+	leftLeftCornerRegion.width = leftPupil.x;
+	leftLeftCornerRegion.height /= 2;
+	leftLeftCornerRegion.y += leftLeftCornerRegion.height / 2;
+	cv::Rect rightLeftCornerRegion(rightEyeRegion);
+	rightLeftCornerRegion.width = rightPupil.x;
+	rightLeftCornerRegion.height /= 2;
+	rightLeftCornerRegion.y += rightLeftCornerRegion.height / 2;
+	cv::Rect rightRightCornerRegion(rightEyeRegion);
+	rightRightCornerRegion.width -= rightPupil.x;
+	rightRightCornerRegion.x += rightPupil.x;
+	rightRightCornerRegion.height /= 2;
+	rightRightCornerRegion.y += rightRightCornerRegion.height / 2;
+	rectangle(debugFace, leftRightCornerRegion, 200);
+	rectangle(debugFace, leftLeftCornerRegion, 200);
+	rectangle(debugFace, rightLeftCornerRegion, 200);
+	rectangle(debugFace, rightRightCornerRegion, 200);
+	// change eye centers to face coordinates
+	rightPupil.x += rightEyeRegion.x;
+	rightPupil.y += rightEyeRegion.y;
+	leftPupil.x += leftEyeRegion.x;
+	leftPupil.y += leftEyeRegion.y;
+	// draw eye centers
+	circle(debugFace, rightPupil, 3, 1234);
+	circle(debugFace, leftPupil, 3, 1234);
+
+	//-- Find Eye Corners
+	if (kEnableEyeCorner) {
+		cv::Point2f leftRightCorner = findEyeCorner(faceROI(leftRightCornerRegion), true, false);
+		leftRightCorner.x += leftRightCornerRegion.x;
+		leftRightCorner.y += leftRightCornerRegion.y;
+		cv::Point2f leftLeftCorner = findEyeCorner(faceROI(leftLeftCornerRegion), true, true);
+		leftLeftCorner.x += leftLeftCornerRegion.x;
+		leftLeftCorner.y += leftLeftCornerRegion.y;
+		cv::Point2f rightLeftCorner = findEyeCorner(faceROI(rightLeftCornerRegion), false, true);
+		rightLeftCorner.x += rightLeftCornerRegion.x;
+		rightLeftCorner.y += rightLeftCornerRegion.y;
+		cv::Point2f rightRightCorner = findEyeCorner(faceROI(rightRightCornerRegion), false, false);
+		rightRightCorner.x += rightRightCornerRegion.x;
+		rightRightCorner.y += rightRightCornerRegion.y;
+		circle(faceROI, leftRightCorner, 3, 200);
+		circle(faceROI, leftLeftCorner, 3, 200);
+		circle(faceROI, rightLeftCorner, 3, 200);
+		circle(faceROI, rightRightCorner, 3, 200);
+	}
+
+	return faceROI;
+	//imshow(face_window_name, faceROI);
+	//  cv::Rect roi( cv::Point( 0, 0 ), faceROI.size());
+	//  cv::Mat destinationROI = debugImage( roi );
+	//  faceROI.copyTo( destinationROI );
+}
+
 Mat detect(Mat input_frame) {
 	vector<Rect> faces;	
 	Mat frame=input_frame;
@@ -238,10 +348,10 @@ Mat detect(Mat input_frame) {
 	for (size_t i = 0; i < faces.size(); i++) {
 		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
 		ellipse(frame, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(0, 255, 0), 4, 8, 0);
-
+		/*
 		Mat faceROI = frame_gray(faces[i]);
 		vector<Rect> eyes;
-
+		
 		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
 		if (eyes.size() == 2){
 			for (size_t j = 0; j < eyes.size(); j++) {
@@ -250,6 +360,11 @@ Mat detect(Mat input_frame) {
 				circle(frame, center, radius, Scalar(0, 0, 255), 4, 8, 0);
 			}
 		}
+		*/
+	}
+	//-- Show what you got
+	if (faces.size() > 0) {
+		frame = findEyes(frame_gray, faces[0]);
 	}
 	return frame;
 }
